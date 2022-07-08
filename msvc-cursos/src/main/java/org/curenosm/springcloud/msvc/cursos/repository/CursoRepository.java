c0d734d9ce3a42b384d94ac65f48fe7a37476eff
Misael Cureño <curenosm@gmail.com>
2022-07-07 20:52:01 -0500

    Avances microservicio cursos

---
+++ msvc-cursos/src/main/java/org/curenosm/springcloud/msvc/cursos/repository/CursoRepository.java
@@ -0,0 +1,8 @@
+package org.curenosm.springcloud.msvc.cursos.repository;
+
+import org.curenosm.springcloud.msvc.cursos.entity.Curso;
+import org.springframework.data.repository.CrudRepository;
+
+public interface CursoRepository extends CrudRepository<Curso, Long> {
+
+}
---
+++ msvc-cursos/src/main/java/org/curenosm/springcloud/msvc/cursos/service/CursoService.java
@@ -0,0 +1,14 @@
+package org.curenosm.springcloud.msvc.cursos.service;
+
+import org.curenosm.springcloud.msvc.cursos.entity.Curso;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface CursoService {
+
+    List<Curso> listar();
+    Optional<Curso> buscarPorId(Long id);
+    Curso guardar(Curso curso);
+    void eliminar(Long id);
+}
