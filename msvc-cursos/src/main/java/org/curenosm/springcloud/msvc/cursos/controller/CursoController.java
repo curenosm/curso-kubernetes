f9d27eaf5b6e91edbdbcd9e90f672342aba99ed7
Misael Cureño <curenosm@gmail.com>
2022-07-07 21:19:21 -0500

    Controlador terminado

--- msvc-cursos/src/main/java/org/curenosm/springcloud/msvc/cursos/controller/CursoController.java
+++ msvc-cursos/src/main/java/org/curenosm/springcloud/msvc/cursos/controller/CursoController.java
@@ -3,10 +3,9 @@ package org.curenosm.springcloud.msvc.cursos.controller;
 import org.curenosm.springcloud.msvc.cursos.entity.Curso;
 import org.curenosm.springcloud.msvc.cursos.service.CursoService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
@@ -32,4 +31,37 @@ public class CursoController {
         return ResponseEntity.notFound().build();
     }
 
+    @PostMapping
+    public ResponseEntity<?> crear(@RequestBody Curso curso) {
+        Curso cursoDB = service.guardar(curso);
+
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(curso);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<?> editar(@RequestBody Curso curso, @PathVariable Long id) {
+        Optional<Curso> o = service.buscarPorId(id);
+
+        if (o.isPresent()) {
+            Curso cursoDB = o.get();
+            cursoDB.setNombre(curso.getNombre());
+            return ResponseEntity.status(HttpStatus.CREATED).body(service.guardar(cursoDB));
+        }
+
+        return ResponseEntity.notFound().build();
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> eliminar(@PathVariable Long id) {
+        Optional<Curso> o = service.buscarPorId(id);
+
+        if (o.isPresent()) {
+            service.eliminar(o.get().getId());
+            return ResponseEntity.noContent().build();
+        }
+
+        return ResponseEntity.notFound().build();
+    }
 }
